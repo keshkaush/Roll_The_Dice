57a3c68a9d6a7ff7775c80fcfc28c4ae8d112761
Your Name <example@example.com>
2020-09-18 18:43:30 +0530

    adding app icons

--- lib/main.dart
+++ lib/main.dart
@@ -27,24 +27,36 @@ class _DicePageState extends State<DicePage> {
   @override
   var leftvalue = 1;
   var rightvalue = 1;
+  double rightWidth = 120;
+  double leftWidth = 260;
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
-          Expanded(
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeInOutQuint,
+            width: rightWidth,
             child: FlatButton(
               onPressed: () {
                 setState(() {
+                  rightWidth = 260;
+                  leftWidth = 120;
                   leftvalue = Random().nextInt(6) + 1;
                 });
               },
               child: Image.asset('images/dice$leftvalue.png'),
             ),
           ),
-          Expanded(
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeInOutQuint,
+            width: leftWidth,
             child: FlatButton(
                 onPressed: () {
                   setState(() {
+                    rightWidth = 120;
+                    leftWidth = 260;
                     rightvalue = Random().nextInt(6) + 1;
                   });
                 },
